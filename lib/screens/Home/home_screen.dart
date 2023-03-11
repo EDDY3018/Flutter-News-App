123e30315576f34c312cbdfe7f906a2fcb5596db
Edmund Quainoo <edmundquainoo3018@gmail.com>
2023-03-11 12:49:37 +0000

    wORKING

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -4,39 +4,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_news_app/screens/Home/widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../specs/colors.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: SizedBox(
-                width: 0,
-                child: GridView.builder(
-                  itemCount: 1,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1),
-                  itemBuilder: (context, index) {
-                    return const HomeHeader();
-                  },
-                ),
-              ),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 1.5,
+            child: GridView.builder(
+              itemCount: 1,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1),
+              itemBuilder: (context, index) {
+                return const HomeHeader();
+              },
             ),
-            Expanded(
-              child: ListView.builder(
-                  padding: EdgeInsets.all(20),
-                  itemCount: 5,
-                  itemBuilder: ((context, index) => Cartegories())),
-            )
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.all(20),
+                itemCount: 1,
+                itemBuilder: ((context, index) => Cartegories())),
+          ),
+        ],
       ),
     );
   }
