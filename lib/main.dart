e9e3ce47957e7c9f720be79fd7ccac03adfce1b6
Ampaw24 <ampawsafo22@gmail.com>
2023-03-14 01:10:56 +0000

    update check

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,6 @@ import 'package:flutter_news_app/screens/Splash/splash.dart';
 void main() {
   runApp(const MyApp());
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 4845d5d1a4236aee12559b8e5b1aadb3641f1045
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
